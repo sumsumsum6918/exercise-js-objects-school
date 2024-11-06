12a073ac82f10002b4924ea364089b7a0b747eff
sum <tinafongfong@gmail.com>
2024-11-06 17:43:11 +0100

    11-13

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ import { subjects } from "./data/subjects.js";
 import { students } from "./data/students.js";
 import { teknikhogskolan } from "./data/school.js";
 
+const callDetails = () => {
+  console.log("School details");
+  console.log(teknikhogskolan);
+  console.log("Subjects details");
+  console.log(subjects);
+  console.log("Students details");
+  console.log(students);
+  console.log("Teachers details");
+  console.log(teachers);
+};
+
 const enrollStudentToSubject = (targetStudent, targetSubject) => {
   subjects.forEach((subject) => {
     if (subject.name !== targetSubject) return; //early return for other subjects
@@ -32,7 +43,7 @@ const enrollAllStudentsToSubject = (targetSubject) => {
     if (subject.name === targetSubject) {
       subject.students = []; //reset array to prevent doubling values and double calling same function
       students.forEach((student) => {
-        subject.addStudent(student);
+        subject.addStudent(student.name);
       });
 
       students.forEach((student) => {
@@ -100,7 +111,7 @@ const removeStudentFromSubject = (targetStudent, targetSubject) => {
   });
 };
 
-const removeTeahcerFromSubject = (targetTeacher, targetSubject) => {
+const removeTeacherFromSubject = (targetTeacher, targetSubject) => {
   subjects.forEach((subject) => {
     if (subject.name !== targetSubject) return; //early return for other subjects
 
@@ -120,40 +131,105 @@ const removeTeahcerFromSubject = (targetTeacher, targetSubject) => {
   });
 };
 
-console.log("===== Start of term =====");
-//#region start of term
-registerAllStudentsToSchool();
-registerAllTeachersToSchool();
-enrollAllStudentsToSubject("mathematics");
-enrollAllStudentsToSubject("mathematics");
-enrollStudentToSubject("student1", "chemistry");
-enrollStudentToSubject("student1", "chemistry");
-enrollStudentToSubject("student2", "chemistry");
-enrollStudentToSubject("student3", "chemistry");
-enrollStudentToSubject("student3", "biology");
-enrollStudentToSubject("student4", "biology");
-enrollStudentToSubject("student5", "biology");
-asignToTeach("teacher1", "mathematics");
-asignToTeach("teacher1", "chemistry");
-asignToTeach("teacher2", "biology");
-asignToTeach("teacher2", "chemistry");
+const studentQuit = (targetName) => {
+  const indexInSchool = teknikhogskolan.students.indexOf(targetName);
+  teknikhogskolan.students.splice(indexInSchool, 1);
 
-console.log("School details");
-console.log(teknikhogskolan);
-console.log("Subjects details");
-console.log(subjects);
-console.log("Students details");
-console.log(students);
-console.log("Teachers details");
-console.log(teachers);
+  const index = students.findIndex((object) => object.name === targetName);
+  const correspondSubjects = students[index].subjects;
+
+  if (index > -1) students.splice(index, 1);
+
+  correspondSubjects.forEach((removingSubject) => {
+    subjects.forEach((subject) => {
+      if (removingSubject !== subject.name) return;
+      if (removingSubject === subject.name) {
+        const index = subject.students.indexOf(targetName);
+        if (index > -1) subject.students.splice(index, 1);
+      }
+    });
+  });
+};
+
+const teacherQuit = (targetName) => {
+  const indexInSchool = teknikhogskolan.teachers.indexOf(targetName);
+  teknikhogskolan.teachers.splice(indexInSchool, 1);
+
+  const index = teachers.findIndex((object) => object.name === targetName);
+  const correspondSubjects = teachers[index].subjects;
+
+  if (index > -1) teachers.splice(index, 1);
+
+  correspondSubjects.forEach((removingSubject) => {
+    subjects.forEach((subject) => {
+      if (removingSubject !== subject.name) return;
+      if (removingSubject === subject.name) {
+        const index = subject.teachers.indexOf(targetName);
+        if (index > -1) subject.teachers.splice(index, 1);
+      }
+    });
+  });
+};
+
+//#region calling functions
+const startOfTermDetails = () => {
+  console.log("===== Start of term =====");
+  startingdetails();
+  callDetails();
+};
+const firstWeekOfTermDetails = () => {
+  console.log(
+    "===== Beginning of term: Students change their choice of subjects ====="
+  );
+  startingdetails();
+  changes1();
+  callDetails();
+};
+const duringTermTime = () => {
+  console.log("===== During term time: Student or teacher quit school =====");
+  startingdetails();
+  changes1();
+  changes2();
+  callDetails();
+};
+const startingdetails = () => {
+  registerAllStudentsToSchool();
+  registerAllTeachersToSchool();
+  enrollAllStudentsToSubject("mathematics");
+  enrollStudentToSubject("student1", "chemistry");
+  enrollStudentToSubject("student1", "chemistry");
+  enrollStudentToSubject("student2", "chemistry");
+  enrollStudentToSubject("student3", "chemistry");
+  enrollStudentToSubject("student3", "biology");
+  enrollStudentToSubject("student4", "biology");
+  enrollStudentToSubject("student5", "biology");
+  asignToTeach("teacher1", "mathematics");
+  asignToTeach("teacher1", "chemistry");
+  asignToTeach("teacher2", "biology");
+  asignToTeach("teacher2", "chemistry");
+};
+const changes1 = () => {
+  removeStudentFromSubject("student1", "chemistry");
+  removeStudentFromSubject("student3", "biology");
+  removeTeacherFromSubject("teacher1", "chemistry");
+};
+const changes2 = () => {
+  studentQuit("student5");
+  teacherQuit("teacher1");
+};
 //#endregion
 
-console.log(
-  "===== Beginning of term: Students change their choice of subjects ====="
-);
-removeStudentFromSubject("student1", "chemistry");
-removeStudentFromSubject("student3", "biology");
-removeTeahcerFromSubject("teacher1", "chemistry");
+/* Since the objects are later modified,
+the console reflects the updated version of objects
+even if the changes was applied after the console.log call
+it is that only one function from below is call at one time.
+Better attempt i believe would be to create new object with .map() when we modify something
+but since the exercise insisted on having functions in the objects,
+I have chosen to aviod the console problem with the method suggested.
+*/
+//startOfTermDetails();
+//firstWeekOfTermDetails();
+//duringTermTime();
 
 /*export const addStudent = (_this, student) => {
   _this.students.push(student);
